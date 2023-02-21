94728116873a33fd37ba754e4ed2cf2736c5250c
Ismail Altay <ismailaltay3553@gmail.com>
2023-02-21 15:57:21 +0300

    Spinner is added

--- packages/terrain-generator/src/pages/index.tsx
+++ packages/terrain-generator/src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Grid, DataProp } from "./components/Grid";
 import { TerrainType } from "./types";
 import { generateValues, generatePerlinValues } from "./utils";
 import { useState, useEffect } from "react";
+import { Spinner } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,10 @@ export default function Home() {
   const height = 300;
   const [values, setValues] = useState<TerrainType[][]>([]);
   const [refresh, setRefresh] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleRefresh = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setIsLoading(true);
     event.preventDefault();
     const values = generatePerlinValues(height, width);
     setValues(values);
@@ -23,18 +26,24 @@ export default function Home() {
     console.log(refresh);
   };
 
+  useEffect(() => {
+    setIsLoading(false);
+  },[values])
+
   return (
     <>
       <div className="flex flex-col justify-center items-center gap-y-20">
         <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           XWar Game Generator
         </h1>
-
-        <div>
+        {
+          isLoading === true ? <h3>Please wait few seconds. Map is being generated...</h3> : 
+          <div>
           {refresh === 0 ? null : (
             <Grid width={width} height={height} values={values} />
           )}
         </div>
+        }
         <div className="flex flex-row">
           <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
             Go
