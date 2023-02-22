1a32086f304aa2b382d29d611e84824925a58fb5
Okan Demir <huseyinokand@gmail.com>
2023-02-22 15:54:21 +0300

    Terrain-Generator is moved

--- packages/terrain-generator/src/pages/test/perlin-map-generator.test.ts
+++
@@ -1,8 +0,0 @@
-import { generatePerlinValues } from "../utils";
-
-describe("Terrain Generation with Perlin Noise", () => {
-  test("it should give the histogram", () => {
-    const intervals = generatePerlinValues(300, 300);
-    console.log(intervals);
-  });
-});
