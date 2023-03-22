67c5e00b647b569d0a2df11a9cac0d72c4871fc5
Hüseyin <huseyinokand@gmail.com>
2023-03-22 20:10:26 +0300

    Webpack problems

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+};
+
+const withTM = require("next-transpile-modules")([
+  "@latticexyz/network",
+  "@latticexyz/react",
+  "@latticexyz/recs",
+  "@latticexyz/std-client",
+  "@latticexyz/utils",
+  "@latticexyz/services"
+]);
+
+module.exports = withTM(nextConfig);
--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,15 +1,28 @@
 import "@/styles/globals.css";
 import "@/styles/style.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from '@chakra-ui/react'
-import { TerrainProvider } from '../context/TerrainContext';
+import { ChakraProvider } from "@chakra-ui/react";
+import { TerrainProvider } from "../context/TerrainContext";
+import { MUDProvider } from "./MudContext";
+import { useEffect, useState } from "react";
+import { setup } from "../mud/setup";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [result, setResult] = useState<any>();
+
+  useEffect(() => {
+    setup().then((res) => {
+      setResult(res);
+    });
+  }, []);
+
   return (
     <ChakraProvider>
-      <TerrainProvider>
-        <Component className="bg-white" {...pageProps} />
-      </TerrainProvider> 
+      <MUDProvider {...result}>
+        <TerrainProvider>
+          <Component className="bg-white" {...pageProps} />
+        </TerrainProvider>
+      </MUDProvider>
     </ChakraProvider>
-  )
+  );
 }
